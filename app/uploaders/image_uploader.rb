5534cde99aa736c1dc96f30ca262c6938b9efc23
ZoOmbu <valerii.bondarenko@nure.ua>
2016-05-23 15:57:42 +0300

    v2.2 adding rspec

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,25 @@
 # encoding: utf-8
 class ImageUploader < CarrierWave::Uploader::Base
   storage :postgresql_lo
+  class FilelessIO < StringIO
+    attr_accessor :original_filename
+    attr_accessor :content_type
+  end
+
+  before :cache, :convert_base64
+
+  def convert_base64(file)
+    if file.respond_to?(:original_filename) &&
+        file.original_filename.match(/^base64:/)
+      fname = file.original_filename.gsub(/^base64:/, '')
+      ctype = file.content_type
+      decoded = Base64.decode64(file.read)
+      file.file.tempfile.close!
+      decoded = FilelessIO.new(decoded)
+      decoded.original_filename = fname
+      decoded.content_type = ctype
+      file.__send__ :file=, decoded
+    end
+    file
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20160521142740) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "image"
+    t.string   "image"
   end
 
 end
